4c0eef62c3721f50d0b3651f6aab18f6db0856e0
https://github.com/Leticiafrnc <leticiafrnc1@gmail.com>
2021-11-09 17:11:15 -0300

    implementacao dos botoes

---
+++ app.js
@@ -0,0 +1,91 @@
+function conferirAbacate() {
+    const respostaAcabate = document.getElementById("respostaAcabate").value;
+
+    if (respostaAcabate == "a-ba-ca-te") {
+        document.getElementById("respAbacate").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respAbacate").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirAbelha() {
+    const respostaAbelha = document.getElementById("respostaAbelha").value;
+
+    if (respostaAbelha == "a-be-lha") {
+        document.getElementById("respAbelha").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respAbelha").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirCachorro() {
+    const respostaCachorro = document.getElementById("respostaCachorro").value;
+
+    if (respostaCachorro == "ca-chor-ro") {
+        document.getElementById("respCachorro").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respCachorro").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirFormiga() {
+    const respostaFormiga = document.getElementById("respostaFormiga").value;
+
+    if (respostaFormiga == "for-mi-ga") {
+        document.getElementById("respFormiga").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respFormiga").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirGato() {
+    const respostaGato = document.getElementById("respostaGato").value;
+
+    if (respostaGato == "ga-to") {
+        document.getElementById("respGato").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respGato").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirLeao() {
+    const respostaLeao = document.getElementById("respostaLeao").value;
+
+    if (respostaLeao == "le-ao") {
+        document.getElementById("respLeao").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respLeao").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirOvelha() {
+    const respostaOvelha = document.getElementById("respostaOvelha").value;
+
+    if (respostaOvelha == "o-ve-lha") {
+        document.getElementById("respOvelha").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respOvelha").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirPolvo() {
+    const respostaPolvo = document.getElementById("respostaPolvo").value;
+
+    if (respostaPolvo == "pol-vo") {
+        document.getElementById("respPolvo").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respPolvo").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+
+function conferirTartaruga() {
+    const respostaTartaruga = document.getElementById("respostaTartaruga").value;
+
+    if (respostaTartaruga == "tar-ta-ru-ga") {
+        document.getElementById("respTartaruga").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respTartaruga").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
